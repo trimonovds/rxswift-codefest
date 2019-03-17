0675e692833584ad6f7c7e5cd9aade664728b116
trimonovds <trimonovds@yandex-team.ru>
2019-03-17 17:37:41 +0300

    Lifecycle: add more slides about BehaviorRelay work when termination event occurs

--- RxPrimitivesLifecycle/RxPrimitivesLifecycle/ViewController.swift
+++ RxPrimitivesLifecycle/RxPrimitivesLifecycle/ViewController.swift
@@ -10,7 +10,22 @@ import UIKit
 import RxSwift
 import RxCocoa
 
+class PrintObserver<T>: ObserverType {
+    typealias E = T
+    deinit { print("PrintObserver<\(T.self)> deallocated") }
+    func on(_ event: Event<T>) { print("PrintObserver<\(T.self)> did receive: \(event)") }
+}
+
 class ViewController: UIViewController {
+override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Try to send on(.completed) to BehaviorRelay
+    Observable<Bool>.empty().bind(to: active)
+
+    let printObserver = PrintObserver<Bool>()
+    _ = active.subscribe(printObserver)
+}
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         active.accept(true)
@@ -21,4 +36,23 @@ class ViewController: UIViewController {
     }
     private let active = BehaviorRelay<Bool>(value: false)
 }
+
+//class ViewController: UIViewController {
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        Observable<Bool>.empty().bind(to: active)
+//        let printObserver = PrintObserver<Bool>()
+//        _ = active.subscribe(printObserver)
+//
+//    }
+//    override func viewDidAppear(_ animated: Bool) {
+//        super.viewDidAppear(animated)
+//        active.onNext(true)
+//    }
+//    override func viewDidDisappear(_ animated: Bool) {
+//        super.viewDidDisappear(animated)
+//        active.onNext(false)
+//    }
+//    private let active = BehaviorSubject<Bool>(value: false)
+//}
 
