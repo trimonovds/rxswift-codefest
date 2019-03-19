86b1290740263d4826d02020cc2533e24732b3f1
trimonovds <trimonovds@yandex-team.ru>
2019-03-19 10:17:12 +0300

    Add Fight with contract slides

---
+++ RxSwift/Rx_iOS.playground/Pages/Fighting with Rx contracts - initial value.xcplaygroundpage/Contents.swift
@@ -0,0 +1,55 @@
+import RxSwift
+import Utils
+import Foundation
+import UIKit
+
+// Before Rx
+
+//protocol ImageLoader {
+//    var image: UIImage? { get }
+//    func loadImage(completion: @escaping (UIImage) -> Void)
+//}
+//
+//class ImageLoadinViewController: UIViewController {
+//    var imageLoader: ImageLoader!
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        if let loaded = imageLoader.image {
+//            imageView.image = loaded
+//        } else {
+//            activityIndicator.startAnimating()
+//            imageLoader.loadImage { [weak self] (img) in
+//                self?.activityIndicator.startAnimating()
+//                self?.imageView.image = img
+//            }
+//        }
+//    }
+//
+//    private let activityIndicator = UIActivityIndicatorView()
+//    private let imageView = UIImageView()
+//}
+
+// After Rx
+
+protocol ImageLoader {
+    func image() -> Observable<UIImage>
+}
+
+class ImageLoadinViewController: UIViewController {
+    var imageLoader: ImageLoader!
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        activityIndicator.startAnimating()
+        imageLoader.image().subscribe(onNext: { [weak self] (img) in
+            self?.activityIndicator.startAnimating()
+            self?.imageView.image = img
+        })
+    }
+
+    private let activityIndicator = UIActivityIndicatorView()
+    private let imageView = UIImageView()
+}
