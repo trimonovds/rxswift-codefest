11d24a30cb736f6c2a1acf44809ee326db8e6e14
trimonovds <trimonovds@yandex-team.ru>
2019-03-30 16:22:05 +0700

    Some improvements. Add qr-code instead of links

--- RxSamples/RxSamplesPlayground.playground/Pages/Lifecycle - subjects.xcplaygroundpage/Contents.swift
+++ RxSamples/RxSamplesPlayground.playground/Pages/Lifecycle - subjects.xcplaygroundpage/Contents.swift
@@ -3,24 +3,28 @@ import RxSwift
 import PlaygroundSupport
 import Utils
 
-func logRxResources() {
-    log("Resources.total: \(Resources.total)")
-}
-
 class PrintObserver<T>: ObserverType {
     typealias E = T
     func on(_ event: Event<T>) { log("PrintObserver<\(T.self)> did receive: \(event)") }
     deinit { log("PrintObserver<\(T.self)> deallocated") }
 }
 
-//let subject = BehaviorSubject<Bool>(value: false)
-//subject.onCompleted()
-//_ = subject.subscribe(PrintObserver<Bool>())
-//subject.onNext(true)
-//subject.onNext(false)
+example("BehaviorSubject behavior") {
+    let subject = BehaviorSubject<Bool>(value: false)
+    subject.onCompleted()
+    _ = subject.subscribe(PrintObserver<Bool>())
+    subject.onNext(true)
+    subject.onNext(false)
+}
 
-let subject = PublishSubject<Bool>()
-subject.onCompleted()
-_ = subject.subscribe(PrintObserver<Bool>())
-subject.onNext(true)
-subject.onNext(false)
+example("PublishSubject behavior") {
+    let subject = PublishSubject<Bool>()
+    subject.onCompleted()
+    _ = subject.subscribe(PrintObserver<Bool>())
+    subject.onNext(true)
+    subject.onNext(false)
+}
+
+
+
+
